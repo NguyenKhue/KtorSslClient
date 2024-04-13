10e9598d3792afd814e1010bd6e4d5652d445f48
khue.nguyen <khue.nguyen@samsung.com>
2024-04-13 18:25:32 +0700

    impl ktor ssl client

---
+++ app/src/main/java/com/khue/ktorsslclient/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.khue.ktorsslclient
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.khue.ktorsslclient.ui.theme.KtorSslClientTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.conscrypt.Conscrypt
+import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.Security
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
+
+@Serializable
+data class Fruit(
+    val id: String,
+    val name: String,
+)
+
+@Serializable
+data class Data(
+    val id: String,
+    val items: List<Fruit>
+)
+
+@Serializable
+data class ResponseData(
+    val data: Data
+)
+
+
+
+class MainActivity : ComponentActivity() {
+
+    fun getKeyStore(): KeyStore {
+        val keyStoreFile = FileInputStream(File(applicationContext.filesDir, "keystore.jks"))
+        val keyStorePassword = "123456".toCharArray()
+        val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        keyStore.load(keyStoreFile, keyStorePassword)
+        return keyStore
+    }
+
+    fun getTrustManagerFactory(): TrustManagerFactory? {
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(getKeyStore())
+        return trustManagerFactory
+    }
+
+    fun getSslContext(): SSLContext? {
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, getTrustManagerFactory()?.trustManagers, null)
+        return sslContext
+    }
+
+    fun getTrustManager(): X509TrustManager {
+        return getTrustManagerFactory()?.trustManagers?.first { it is X509TrustManager } as X509TrustManager
+    }
+
+    suspend fun getFruitFromApi(): ResponseData {
+        val client = HttpClient(Android) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+                sanitizeHeader { header -> header == HttpHeaders.Authorization }
+            }
+            install(ContentNegotiation) {
+                json(json)
+            }
+            engine {
+                sslManager = { httpsURLConnection ->
+                    httpsURLConnection.sslSocketFactory = getSslContext()?.socketFactory
+                }
+            }
+        }
+
+        return client.get("https://10.1.140.124:8443/fruits").body()
+    }
+
+    companion object {
+        private val json = Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+            isLenient = true
+            encodeDefaults = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Security.insertProviderAt(Conscrypt.newProvider(), 1)
+        CoroutineScope(Dispatchers.IO).launch {
+            findFruit()
+        }
+        setContent {
+            KtorSslClientTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    private suspend fun findFruit() {
+        val fruits = getFruitFromApi()
+        Log.d("KtorSslClient", fruits.toString())
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KtorSslClientTheme {
+        Greeting("Android")
+    }
+}
